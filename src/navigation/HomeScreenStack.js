0d1edb663f41562182d337fa118ae5ae82d1b466
lawtlee <lawsac2947@gmail.com>
2023-04-28 10:33:21 -0700

    Separated Navigational Stacks
    Moved NavbarVisible, HomeScreenStack, SignInFlow
    into their respective files in ./src/navigation.
    Converted them from function to const. Also fixed
    some dependency issues.

---
+++ src/navigation/HomeScreenStack.js
@@ -0,0 +1,24 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen.js";
+import ConcertScreen from "../screens/ConcertScreen.js";
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+export const HomeScreenStack = (route) => {
+    return (
+        <Stack.Navigator headerMode="false">
+            <Tab.Screen
+                name="Home Screen"
+                component={HomeScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="Concert Screen"
+                component={ConcertScreen}
+                options={{ headerShown: false }}
+            />
+        </Stack.Navigator>
+    );
+}
