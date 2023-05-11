dee7d7dcfc02bbcf573aca44d8d396b15a94e669
Ajzboss <ajano3191@gmail.com>
2023-05-10 20:36:11 -0700

    Added MUI functionality to FriendRequestBlock.js

--- src/components/common/FriendRequestBlock.js
+++ src/components/common/FriendRequestBlock.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
+import { View, StyleSheet, Text, Image,TouchableOpacity } from "react-native";
 import { Colors, Dim } from "../../Constants";
+import { Button } from "@react-native-material/core";
+import { color } from "@rneui/base";
 //
 /*
   -- DOCUMENTATION --
@@ -8,13 +10,17 @@ import { Colors, Dim } from "../../Constants";
 const FriendRequestBlock = (props) => {
     return (
         <View style={styles.container}>   
-            <Image style={{ width: 50, height: 50, borderRadius: 50, marginLeft: 10, marginTop: 10, marginBottom: 10,marginRight: 20,}}
+            <Image style={{ width: 50, height: 50, borderRadius: 50, marginLeft: 10, marginTop: 10, marginBottom: 10,marginRight: 10,}}
                 source={{
                     uri: props.image,
                 }}
                 />
             <Text style={styles.header}>{props.name}
             <Text style={styles.subheader}>{"\n @"}{props.username} </Text></Text>
+            <View style={styles.ButtonView}>
+            <TouchableOpacity style={styles.roundedButton}><Text style={styles.buttonText}>Accept</Text></TouchableOpacity>
+                <TouchableOpacity ><Text style={{fontSize: 20}}>X</Text></TouchableOpacity>
+                </View>
         </View>
     );
 };
@@ -29,9 +35,26 @@ const styles = StyleSheet.create({
         marginLeft: 20,
         backgroundColor: "#f0f0f0",
 },
+ButtonView: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    width:Dim.width*0.5,
+    },
 
     header: {
         fontSize: 20,
+    },
+    buttonText: {
+        color: "white",
+    },
+    roundedButton: {   
+        width: 100,
+        height: 30,
+        backgroundColor: "#0DAD81",
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        
     },
 subheader: {    
     color: Colors.darkGray,
